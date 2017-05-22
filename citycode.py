fac1861df8f4d27f5c91ff6c27cb63aceca82782
tattii <yuta.tatti@gmail.com>
2017-05-22 19:14:54 +0900

    add pref, distlict, division

--- citycode.py
+++ citycode.py
@@ -4,6 +4,8 @@ import re
 
 
 def getlist():
+    arealist = getarealist()
+
     with codecs.open('jma-definition/CityCode.csv', encoding='utf-8') as f:
         f.readline() # header
         data = {}
@@ -13,17 +15,45 @@ def getlist():
 
             if row[5] == '1': # 気象警報で使用
                 split = re.match(u"気象庁予報警報規程別表", row[1]) is not None
-                data[row[0]] = {
-                    'name': row[2],
-                    'parentArea': row[4],
-                    'splitArea': split
-                }
+
+                code = row[0]
+                data[code] = arealist[row[4]]
+
+                data[code]['name'] = row[2]
+                data[code]['splitArea'] = split
 
         return data
 
 
+def getarealist():
+    '''
+    prefecture 府県予報区
+    distlict   一次細分
+    division   市町村等をまとめた地域
+    city       二次細分
+    '''
+    with codecs.open('jma-definition/AreaForecastLocal.csv', encoding='utf-8') as f:
+        f.readline() # header
+        area = {}
+
+        for line in f:
+            row = line.split(',')
+
+            area[row[0]] = {
+                'divisionCode': row[0],
+                'divisionName': row[1],
+                'distlictCode': row[2],
+                'distlictName': row[3],
+                'prefCode': row[4],
+                'prefName': row[5],
+            }
+
+        return area
 
 if __name__ == '__main__':
-    print getlist()
+    cities = getlist()
+    for l in cities.keys():
+        c = cities[l]
+        print ','.join([l, c['name'], str(c['splitArea']), c['divisionName'], c['distlictName'], c['prefName']])
 
 
