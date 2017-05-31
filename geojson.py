c2d864ec1ae1fde45dfa2db79d9dbdb1df31d801
tattii <yuta.tatti@gmail.com>
2017-06-01 03:26:13 +0900

    output geojson

--- geojson.py
+++ geojson.py
@@ -8,6 +8,10 @@ import citycode
 def main():
     citylist = citycode.getlist()
 
+    cities = load_geojson(citylist)
+    output_geojson(cities, citylist)
+
+def load_geojson(citylist):
     with open('data/N03-170101_GML/japan2016.json') as f:
         data = json.loads(f.read(), 'utf-8')
         cities = {}
@@ -36,13 +40,31 @@ def main():
                     #print city_name, name
                     pass
 
-    print len(cities)
+        return cities
+
+def output_geojson(cities, citylist):
+    print 'cities:' + str(len(cities))
+    for code in cities.keys():
+        if code == '0120200':
+            create_geojson(code, cities[code])
+
 
 def append_feature(cities, code, feature):
     if not code in cities:
         cities[code] = []
 
     cities[code].append(feature)
+
+
+def create_geojson(code, feature):
+    geojson = {
+        'type': 'FeatureCollection',
+        'features': feature
+    }
+
+    with open(code + '.json', 'w') as f:
+        json_str = json.dumps(geojson, ensure_ascii=False)
+        f.write(json_str.encode('utf-8'))
 
 
 if __name__ == '__main__':
