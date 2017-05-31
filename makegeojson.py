58582ba6abf2d581ab30c366dd86072528d5cfa0
tattii <yuta.tatti@gmail.com>
2017-06-01 06:54:50 +0900

    add designated city

--- makegeojson.py
+++ makegeojson.py
@@ -31,12 +31,12 @@ def load_geojson(citylist):
                 continue
 
             jma_code = str(code) + '00'
-            parent_code = str(code[:3]) + '0000'
 
             if jma_code in citylist:
                 append_geometry(cities, jma_code, feature)
 
             else:
+                parent_code = get_parent_code(str(code))
                 if parent_code in citylist:
                     append_geometry(cities, parent_code, feature)
 
@@ -45,6 +45,32 @@ def load_geojson(citylist):
                     pass
 
         return cities
+
+def get_parent_code(code):
+    '''
+    政令指定都市 区がある
+    '''
+
+    second_designated_city = [
+        '1413', # 川崎市
+        '1415', # 相模原市
+        '2714', # 堺市
+        '4010', # 北九州市
+        '4013', # 福岡市
+    ]
+
+    if code[:4] in second_designated_city:
+        return code[:4] + '000'
+
+    elif code[:4] == '2213': # 浜松市
+        if code == '2213700':
+            return '2213002' # 浜松市北部
+        else:
+            return '2213001' # 浜松市南部
+
+    else: # first designated city
+        code[:3] + '0000'
+
 
 def output_geojson(cities, citylist):
     print 'cities:' + str(len(cities))
