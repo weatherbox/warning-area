05dbe41b0fd136ca19135f6fcd8b518b491f68b8
tattii <yuta.tatti@gmail.com>
2017-06-09 00:05:15 +0900

    add attribution

--- map.js
+++ map.js
@@ -42,7 +42,8 @@ $(function(){
 			"type": "vector",
 			"minzoom": 0,
 			"maxzoom": 10,
-			"tiles": ["https://s3-ap-northeast-1.amazonaws.com/vector-tile/warning-area" + source_suffix + "/{z}/{x}/{y}.pbf"]
+			"tiles": ["https://s3-ap-northeast-1.amazonaws.com/vector-tile/warning-area" + source_suffix + "/{z}/{x}/{y}.pbf"],
+			"attribution": '<a href="http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03-v2_3.html" target="_blank">国土数値情報</a>'
 		});
 
 		map.addLayer({
